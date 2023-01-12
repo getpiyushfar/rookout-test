f5e3e9c3d550716417833b0fc18ffeabd6f67b2d
getpiyushfar <piyush.chauhan@getfareye.com>
2023-01-12 12:34:28 +0530

    Rookout and express added

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const rookout = require('rookout')
+const express = require('express')
+const app = express()
+const port = 3000
+rookout.start({
+    token: '48c487135cb7261785ee93623ec9a19f65ec54244882ed1090829fc5b9c68c06',labels: {env: 'dev'}})
 function caller(a) {
     console.log(a)
 }
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 console.log('Hello this is Piyush')
 caller('zskdhfzkdsfj')
